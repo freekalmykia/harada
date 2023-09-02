64a33c02b24169a20e6cba7d8b16432724efd0bd
AlSharp <elistaphilly@gmail.com>
2023-09-02 11:28:09 -0400

    change layout title

--- web/src/layout/Layout.jsx
+++ web/src/layout/Layout.jsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router-dom"
 export default function Layout() {
   return (
     <div>
-      <h1 className="text-3xl">== Harada ==</h1>
+      <h1 className="text-3xl">!! Harada !!</h1>
       <nav>
         <ul>
           <li>
