a4ecab1fac2eee590792f2734c1ef7862e683cf3
AlSharp <elistaphilly@gmail.com>
2023-09-02 12:51:21 -0400

    display posts

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,3 +1,31 @@
-export default function() {
-  return <h1>Home</h1>
+import { useState, useEffect } from "react"
+import Post from "../components/Post";
+
+export default function Home() {
+
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async (url) => {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  }
+
+  const renderPosts = posts => {
+    return posts.map(post => <Post key={post.id} post={post} />)
+  }
+
+  useEffect(() => {
+    getPosts('http://localhost:3000/mockdata/posts.json')
+      .then(posts => {
+        setPosts(posts);
+      })
+      .catch(error => console.log(error))
+  }, [])
+
+  return (
+    <div>
+      { renderPosts(posts) }
+    </div>
+  )
 }
